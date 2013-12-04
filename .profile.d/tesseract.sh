06771dd18993b534cfa9f8494ff536c2d7349030
Neamar <neamar@neamar.fr>
2013-12-04 11:53:50 +0100

    Prefix

--- .profile.d/tesseract.sh
+++ .profile.d/tesseract.sh
@@ -2,4 +2,4 @@
 
 # We have a local installation of tessrract
 # We need to manually provide tessdata path
-export TESSDATA_PREFIX=~/.apt/usr/share/tesseract-ocr/tessdata
+export TESSDATA_PREFIX=~/.apt/usr/share/tesseract-ocr
