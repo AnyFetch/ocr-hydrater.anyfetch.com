7db4f7d65b0cb81e9c2c45fd44ed4bff9e37ca5a
Neamar <neamar@neamar.fr>
2014-01-22 13:42:51 +0100

    Cleaning up files

--- .profile.d/tesseract.sh
+++ .profile.d/tesseract.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# We have a local installation of tessrract
+# We have a local installation of tesseract
 # We need to manually provide tessdata path
 export TESSDATA_PREFIX=~/.apt/usr/share/tesseract-ocr/
