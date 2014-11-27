2552ef2e683020e80884bdb7b339b64f81d25ad3
Amoki <hugoduroux@gmail.com>
2014-11-27 13:56:20 +0100

    update config

--- config/configuration.js
+++ config/configuration.js
@@ -2,21 +2,23 @@
  * @file Defines the hydrater settings.
  */
 
-// node_env can either be "development" or "production"
-var node_env = process.env.NODE_ENV || "development";
-var default_port = 8000;
+// nodeEnv can either be "development" or "production"
+var nodeEnv = process.env.nodeEnv || "development";
+var defaultPort = 8000;
 
 // Number of tesseract instance to run simultaneously per cluster
-var default_concurrency = 1;
+var defaultConcurrency = 1;
 
-if(node_env === "production") {
-  default_port = 80;
+if(nodeEnv === "production") {
+  defaultPort = 80;
 }
 
 // Exports configuration
 module.exports = {
-  env: node_env,
-  port: process.env.PORT || default_port,
+  env: nodeEnv,
+  port: process.env.PORT || defaultPort,
 
-  concurrency: process.env.TESSERACT_CONCURRENCY || default_concurrency
+  concurrency: process.env.TESSERACT_CONCURRENCY || defaultConcurrency,
+  redisUrl: process.env.REDIS_URL,
+  appName: process.env.APP_NAME || "ocr-hydrater"
 };
